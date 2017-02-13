e043d0c2b6a34a97d1b0fffd4e1b24d32bed4416
Christian Hubinger <christian@trigo.at>
2017-02-14 00:08:43 +0100

    feature: migration & db setup
    Add config section "migrations" to setup the migration manager and run
    all pending migrations on startup when configured to do so using thw
    "runOnStartup" setting.
    
    Add "createDb" setting.
    When "true" and the configured database does
    not exist, the plugin tires to create the databse using the credentials
    confgured in the server section.
    When "false" and the database does not exist an error if thown on
    start-up

--- specs/connect.specs.js
+++ specs/connect.specs.js
@@ -3,10 +3,7 @@
 /* eslint-env node, mocha */
 /* eslint no-unused-expressions: 0, arrow-body-style: 0 */
 
-const fs = require('fs');
-const path = require('path');
 const { expect } = require('chai');
-const OrientDB = require('orientjs');
 
 require('./service');
 const atrix = require('@trigo/atrix');
@@ -24,22 +21,43 @@ describe('loads datasources into service', () => {
 		expect(atrix.services.orientdb.dataConnections.m1.getServer).to.be.an('function');
 	});
 
-	beforeEach(async () => {
+	after(async () => {
 		const server = atrix.services.orientdb.dataConnections.m1.getServer();
 		const dbs = await server.list();
 		await Promise.all(dbs.map(db => server.drop(db.name)));
 		await server.close();
 	});
 
-	it('can create a new databse using a server instance', async () => {
-		const server = atrix.services.orientdb.dataConnections.m1.getServer();
-		const db = await server.create({
-			name: 'atrix-orientdb-test',
-			type: 'graph',
-			storage: 'memory',
+	describe('db creation', () => {
+		it('creates the database on startup', async () => {
+			const server = atrix.services.orientdb.dataConnections.m1.getServer();
+			const exists = await server.exists(atrix.services.orientdb.config.config.dataSource.m1.config.db.name);
+			expect(exists).to.be.true;
+			server.close();
 		});
-		expect(db).to.be.an.instanceof(OrientDB.Db);
-		await server.close();
+
+		it('throws on start when db does not exist but createDb == false', async () => {
+			try {
+				await atrix.services.orientdb2.start();
+				throw new Error('Should hav thrown');
+			} catch (e) {
+				expect(e.message).to.contain('"ned-do"');
+			}
+		});
+	});
+
+	describe('migrations', () => {
+		it('no error when no migrations config section is omitted', async () => {
+			await atrix.services.orientdb3.start();
+		});
+
+		it('runs all migrations on startup when "config.db.migrations.runOnStartup = true"', async () => {
+			const db = atrix.services.orientdb.dataConnections.m1.db;
+			const Test = await db.class.get('Test');
+			expect(Test).to.exist;
+			const Test2 = await db.class.get('Test2');
+			expect(Test2).to.exist;
+		});
 	});
 });
 
--- specs/service.js
+++ specs/service.js
@@ -21,8 +21,62 @@ const svc = new atrix.Service('orientdb', {
 					username: 'root',
 					password: 'password',
 				},
+				migrations: {
+					dir: path.join(__dirname, './test_migrations'),
+					runOnStartup: true,
+				},
+				createDb: true,
 			},
 		},
 	},
 });
 atrix.addService(svc);
+
+const svc2 = new atrix.Service('orientdb2', {
+	dataSource: {
+		m1: {
+			type: 'orientdb',
+			config: {
+				server: {
+					host: '127.0.0.1',
+					port: 2424,
+					username: 'root',
+					password: 'password',
+				},
+				db: {
+					name: 'ned-do',
+					username: 'root',
+					password: 'password',
+				},
+				migrations: {
+					dir: path.join(__dirname, './test_migrations'),
+					runOnStartup: true,
+				},
+				createDb: false,
+			},
+		},
+	},
+});
+atrix.addService(svc2);
+const svc3 = new atrix.Service('orientdb3', {
+	dataSource: {
+		m1: {
+			type: 'orientdb',
+			config: {
+				server: {
+					host: '127.0.0.1',
+					port: 2424,
+					username: 'root',
+					password: 'password',
+				},
+				db: {
+					name: 'andere',
+					username: 'root',
+					password: 'password',
+				},
+				createDb: true,
+			},
+		},
+	},
+});
+atrix.addService(svc3);
